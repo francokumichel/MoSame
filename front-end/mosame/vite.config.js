450046ffe0dbf66079165cba758a7abc8ce37cf8
Franco Kumichel <francokumichel1996@gmail.com>
2026-02-10 02:25:35 -0300

    fix: configuro proxy en el front

--- front-end/mosame/vite.config.js
+++ front-end/mosame/vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -11,6 +10,17 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    port: 8080,
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      }
     }
   }
 })
